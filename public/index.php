c98e435f3a400f5bc85f829a1299c6f59dd4778d
Ernesto Orellana <ernestoorellana5267@gmail.com>
2022-02-16 13:45:48 -0600

    EStructura

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * @author Ernesto Orellana <eorellana5c@gmail.com>
+ * @date 16/02/2022 - 13:42
+ */
+
+
---
+++ resources/templates/footer.php
@@ -0,0 +1,5 @@
+<?php
+/**
+ * @author Ernesto Orellana <eorellana5c@gmail.com>
+ * @date 16/02/2022 - 13:43
+ */
